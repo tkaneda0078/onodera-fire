f55d64bcd4946cc89e85cf0885baa850d46930d0
tkaneda0078 <1111510078r21@gmail.com>
2018-07-03 19:43:40 +0900

    test commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+$sign = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $sign);
+
+foreach ($events as $event) {
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent) ||
+      !($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+    continue;
+  }
+  $bot->replyText($event->getReplyToken(), $event->getText());
+}
