462caa5a73811ac22aed94ca75e1aa68c16462ad
tkaneda0078 <1111510078r21@gmail.com>
2018-07-09 21:42:42 +0900

    log

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 $sign = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $sign);
 
+error_log(2222222222);
+
 foreach ($events as $event) {
   if (!($event instanceof \LINE\LINEBot\Event\MessageEvent) ||
     !($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
