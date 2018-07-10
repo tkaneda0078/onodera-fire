585bf62f0804e8b96534fb002233f1005d59e212
tkaneda0078 <1111510078r21@gmail.com>
2018-07-10 14:15:19 +0900

    push用

---
+++ pushMessage.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use \LINE\LINEBot;
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot\Constant\HTTPHeader;
+
+$httpClient = new CurlHTTPClient(getenv('LINE_CHANNEL_ACCESS_TOKEN'));
+$bot = new LINEBot($httpClient, ['channelSecret' => getenv('LINE_CHANNEL_SECRET')]);
+
+$pushMessage = 'push onodera';
+
+$response = $bot->pushMessage(getenv('LINE_USER_ID'), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($pushMessage));
+
+if (!$response->isSucceeded()) {
+  error_log('Faild!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
+}
